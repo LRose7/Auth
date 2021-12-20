c0dd87d17532aaf85b631a14181efdd5bdf63770
LRose7 <lindseyurquhart@tutanota.com>
2021-12-20 07:45:08 -0500

    added routes files

--- api/index.js
+++ api/index.js
@@ -1,22 +1,37 @@
+if (process.env.NODE_ENV = 'production') {
+    require('dotenv').config();
+}
+
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config();
+
+const usersRoute = require('./routes/users');
+const profileRoute = require('./routes/profile');
+const postsRoute = require('./routes/posts');
 
 // set up express app
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello World')
-})
+});
 
-const PORT = process.env.PORT || 5000
+// Routes
+app.use('/api/users', usersRoute);
+app.use('/api/posts', postsRoute);
+app.use('/api/profile', profileRoute);
+
+const connection_string = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
     console.log(`Server is listening on port: ${PORT}`);
 });
 
 // connect to MongoDB
-const connection_string = process.env.MONGODB_URI;
-mongoose
-.connect(connection_string)
-.then(() => console.log('MongoDB Connection Established'))
-.catch(err => console.log(err));
+mongoose.connect(connection_string, {    
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB Connection Established..."))
+.catch(error => console.error("MongoDB Connection Failed: ", error.message));
---
+++ api/routes/profile.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// @route   GET api/profile
+// @desc    Tests profile route
+// @access  Public
+router.get('/', (req, res) => {
+    res.json({ msg: 'Profile Works' });
+});
+
+module.exports = router;
