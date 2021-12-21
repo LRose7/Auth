3b0f30bcebb2f9576baab3d69bced07a4af61d8f
LRose7 <lindseyurquhart@tutanota.com>
2021-12-21 13:10:11 -0500

    added react-router-dom and Links

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,29 @@
-import './App.css';
+import React, { Component } from 'react'
+import { 
+  BrowserRouter as Router,
+  Routes, 
+  Route 
+} from 'react-router-dom'
+
 import Footer from './components/layout/footer/Footer';
 import Landing from './components/layout/landing/Landing';
 import Navbar from './components/layout/navbar/Navbar';
-function App() {
-  return (
-    <>
-      <Navbar />
-      <Landing />
-      <Footer />
-    </>
-  );
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={ <Landing /> } />
+          <Route path="/register" element={ <Register />} />
+          <Route path="/login" element={ <Login />} />
+        </Routes>
+        <Footer />
+      </Router>
+    )
+  }
 }
 
-export default App;
--- client/src/components/layout/navbar/Navbar.jsx
+++ client/src/components/layout/navbar/Navbar.jsx
@@ -1,33 +1,35 @@
 import React, { Component } from 'react';
 import './navbar.css';
 
+import { Link } from 'react-router-dom';
+
 // Using class based component because will have Life cycle methods  
 class Navbar extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
                 <div className="container">
-                    <a href="" className="navbar-brand">DevConnector</a>
-                    <button className="navbar-toggle">
-                        <span className="navbar-toggle-icon"></span>
+                    <Link to="/" className="navbar-brand">DevConnector</Link>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
 
                     <div className="collapse navbar-collapse" id="mobile-nav">
                         <ul className="navbar-nav mr-auto">
                             <li className="nav-item">
-                                <a href="" className="nav-link">Developers</a>
+                                <Link to="/profiles" className="nav-link">Developers</Link>
                             </li>
                         </ul>
 
-                        <ul className="navbar-nav mr-auto">
+                        <ul className="navbar-nav ms-auto">
                             <li className="nav-item">
-                                <a href="" className="nav-link">Sign Up</a>
+                                <Link to="/register" className="nav-link">Sign Up</Link>
                             </li>
                         </ul>
 
-                        <ul className="navbar-nav mr-auto">
+                        <ul className="navbar-nav ml-auto">
                             <li className="nav-item">
-                                <a href="" className="nav-link">Login</a>
+                                <Link to="/login" className="nav-link">Login</Link>
                             </li>
                         </ul>
                     </div>
